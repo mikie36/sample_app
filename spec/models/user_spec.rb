0b1c19502020d78a1c1efa6eba81e527871900a8
AFSHARI <mikie36@gmail.com>
2014-05-21 17:03:59 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ before do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -84,5 +86,9 @@ describe "when email address is already taken" do
     end
 
     it { should_not be_valid }
+  end
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
